c0caa152cc84c8d2cb5c45b6ad122b868e536447
Michael Kirschner <mjk.kirschner@gmail.com>
2024-01-08 22:58:38 -0500

    fix bug where loading package does not re-execute graph. (#4)
    * appdomain for deps
    
    * load engine ourselves
    
    * review comments
    
    * when extension is late loaded run the graph to resolve the new engine.
    
    * test issues

---
+++ DSironPythonEmpty/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace DSironPythonEmpty
+{
+    /// <summary>
+    /// This class/dll exists so that Dynamo knows this package is loaded,
+    /// even though it really only contains an extension.
+    /// </summary>
+    internal class Class1
+    {
+
+    }
+}
--- IronPythonExtension/IronPythonExtension.cs
+++ IronPythonExtension/IronPythonExtension.cs
@@ -1,10 +1,12 @@
-﻿using System;
-using System.IO;
-using System.Reflection;
-using System.Runtime.Loader;
-using Dynamo.Extensions;
+﻿using Dynamo.Extensions;
+using Dynamo.Graph.Workspaces;
 using Dynamo.Logging;
 using Dynamo.PythonServices;
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
 
 namespace IronPythonExtension
 {
@@ -59,27 +61,87 @@ namespace IronPythonExtension
         /// Action to be invoked when the Dynamo has started up and is ready
         /// for user interaction. 
         /// </summary>
-        /// <param name="sp"></param>
-        public void Ready(ReadyParams sp)
+        /// <param name="rp"></param>
+        public void Ready(ReadyParams rp)
         {
-            var extraPath = Path.Combine(new FileInfo(Assembly.GetAssembly(typeof(IronPythonExtension)).Location).Directory.Parent.FullName, "extra");
-            var alc = new IsolatedPythoContext(Path.Combine(extraPath,"DSIronPython.dll"));
-            alc.LoadFromAssemblyName(new AssemblyName("DSIronPython"));
-        }
+            var extraPath = Path.Combine(new FileInfo(Assembly.GetExecutingAssembly().Location).Directory.Parent.FullName, "extra");
+            var alc = new IsolatedPythonContext(Path.Combine(extraPath,$"{PythonEvaluatorAssembly}.dll"));
+            var dsIronAssem = alc.LoadFromAssemblyName(new AssemblyName(PythonEvaluatorAssembly));
 
-        /// <summary>
-        /// Action to be invoked when shutdown has begun.
-        /// </summary>
+            //load the engine into Dynamo ourselves.
+            LoadPythonEngine(dsIronAssem);
+            
+            //we used to do this:
+            //but it's not neccesary to load anything into the VM.
+            //instead we skip all the extra work and trigger the side effect we want
+            //which is re executing the graph after the dsIronPython evaluator is loaded into the PythonEngineManager.
+            //rp.StartupParams.LibraryLoader.LoadNodeLibrary(dsIronAssem);
+
+            if(rp.CurrentWorkspaceModel is HomeWorkspaceModel hwm)
+            {
+                foreach (var n in hwm.Nodes)
+                {
+                    n.MarkNodeAsModified(true);
+                }
+                hwm.Run();
+            }
+        }
         public void Shutdown()
         {
             // Do nothing for now
         }
+
+        private static void LoadPythonEngine(Assembly assembly)
+        {
+            if (assembly == null)
+            {
+                return;
+            }
+
+            // Currently we are using try-catch to validate loaded assembly and Singleton Instance method exist
+            // but we can optimize by checking all loaded types against evaluators interface later
+            try
+            {
+                Type eType = null;
+                PropertyInfo instanceProp = null;
+                try
+                {
+                    eType = assembly.GetTypes().FirstOrDefault(x => typeof(PythonEngine).IsAssignableFrom(x) && !x.IsInterface && !x.IsAbstract);
+                    if (eType == null) return;
+
+                    instanceProp = eType?.GetProperty("Instance", BindingFlags.NonPublic | BindingFlags.Static);
+                    if (instanceProp == null) return;
+                }
+                catch
+                {
+                    // Ignore exceptions from iterating assembly types.
+                    return;
+                }
+
+                PythonEngine engine = (PythonEngine)instanceProp.GetValue(null);
+                if (engine == null)
+                {
+                    throw new Exception($"Could not get a valid PythonEngine instance by calling the {eType.Name}.Instance method");
+                }
+
+                if (PythonEngineManager.Instance.AvailableEngines.All(x => x.Name != engine.Name))
+                {
+                    PythonEngineManager.Instance.AvailableEngines.Add(engine);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Failed to add a Python engine from assembly {assembly.GetName().Name}.dll with error: {ex.Message}");
+            }
+        }
+
+
     }
-    internal class IsolatedPythoContext : AssemblyLoadContext
+    internal class IsolatedPythonContext : AssemblyLoadContext
     {
         private AssemblyDependencyResolver resolver;
 
-        public IsolatedPythoContext(string libPath)
+        public IsolatedPythonContext(string libPath)
         {
             resolver = new AssemblyDependencyResolver(libPath);
         }
